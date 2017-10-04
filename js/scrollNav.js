ea6b7fe5df22c8164b8092d274ff8d9170696621
Robson Daniel <Robson-Daniel@outlook.com>
2017-10-04 00:50:27 -0300

    Update scrollNav.js

--- js/scrollNav.js
+++ js/scrollNav.js
@@ -1,25 +1,15 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".nav a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
+$(document).ready(function() {
+  $(".nav a").click(function(event) {
+
+    if(this.hash !== ""){
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // console.log(hash);
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
+      $("html, body").animate({
         scrollTop: $(hash).offset().top
-      }, 900, function(){
+      },900);
+    }
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        // window.location.hash = hash;
-      });
-    } // End if
   });
 });
